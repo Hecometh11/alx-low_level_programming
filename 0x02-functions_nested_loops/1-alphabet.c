459fdd350a489d29ddd398fb725cd6855eab25e4
Hecometh11 <tomisinoloyede1@gmail.com>
2022-12-13 15:02:08 -0800

    Alphabeth game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all in lower case
+ */
+
+void print_alphabeth(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	-putchar('\n');
+}
