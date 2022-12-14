d70ca5fd2947a0d9cb301dd91565f416807d0baf
Hecometh11 <tomisinoloyede1@gmail.com>
2022-12-14 15:51:09 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - print _putchar
- * is written by hecometh
- * Return: 0
+ * main - prints _putchar, followed by a new line
+ * Return: Always (0)
  */
 int main(void)
 {
@@ -16,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
-
